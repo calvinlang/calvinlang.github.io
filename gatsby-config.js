099fec1a4fc48454a2e3c8901441f486d0dee7ae
Calvin Lang <langcalvin@gmail.com>
2020-08-27 18:28:18 +0800

    Updated Config added image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,51 +1,49 @@
-const lost = require('lost')
-const pxtorem = require('postcss-pxtorem')
+const lost = require("lost")
+const pxtorem = require("postcss-pxtorem")
 
-const url = 'https://lumen.netlify.com'
+const url = "https://calvinlang.github.io/"
 
 module.exports = {
   siteMetadata: {
     url,
     siteUrl: url,
-    title: 'Blog by John Doe',
-    subtitle:
-      'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-    copyright: '© All rights reserved.',
-    disqusShortname: '',
+    title: "Mental Belly Rubs",
+    subtitle: "A place for me to practice my drawing",
+    copyright: "© All rights reserved.",
+    disqusShortname: "",
     menu: [
       {
-        label: 'Articles',
-        path: '/',
+        label: "Articles",
+        path: "/",
       },
       {
-        label: 'About me',
-        path: '/about/',
-      },
-      {
-        label: 'Contact me',
-        path: '/contact/',
-      },
+        label: "About me",
+        path: "/about/",
+      }, // {
+      //   label: "Contact me",
+      //   path: "/contact/",
+      // },
     ],
     author: {
-      name: 'John Doe',
-      email: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '#',
-      vk: '#',
+      name: "Calvin Lang",
+      email: "langcalvin@gmail.com",
+      telegram: "#",
+      twitter: "#",
+      github: "calvinlang",
+      rss: "#",
+      vk: "#",
     },
   },
   plugins: [
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
+        name: "pages",
       },
     },
     {
-      resolve: 'gatsby-plugin-feed',
+      resolve: "gatsby-plugin-feed",
       options: {
         query: `
           {
@@ -67,7 +65,7 @@ module.exports = {
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.url + edge.node.fields.slug,
                   guid: site.siteMetadata.url + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                  custom_elements: [{ "content:encoded": edge.node.html }],
                 })
               ),
             query: `
@@ -95,49 +93,49 @@ module.exports = {
                 }
               }
             `,
-            output: '/rss.xml',
+            output: "/rss.xml",
           },
         ],
       },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 960,
             },
           },
           {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: { wrapperStyle: 'margin-bottom: 1.0725rem' },
+            resolve: "gatsby-remark-responsive-iframe",
+            options: { wrapperStyle: "margin-bottom: 1.0725rem" },
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
         ],
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-73379983-2' },
+      resolve: "gatsby-plugin-google-analytics",
+      options: { trackingId: "UA-73379983-2" },
     },
     {
-      resolve: 'gatsby-plugin-google-fonts',
+      resolve: "gatsby-plugin-google-fonts",
       options: {
-        fonts: ['roboto:400,400i,500,700'],
+        fonts: ["roboto:400,400i,500,700"],
       },
     },
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-offline',
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-plugin-sass',
+      resolve: "gatsby-plugin-sass",
       options: {
         postCssPlugins: [
           lost(),
@@ -145,23 +143,23 @@ module.exports = {
             rootValue: 16,
             unitPrecision: 5,
             propList: [
-              'font',
-              'font-size',
-              'line-height',
-              'letter-spacing',
-              'margin',
-              'margin-top',
-              'margin-left',
-              'margin-bottom',
-              'margin-right',
-              'padding',
-              'padding-top',
-              'padding-left',
-              'padding-bottom',
-              'padding-right',
-              'border-radius',
-              'width',
-              'max-width',
+              "font",
+              "font-size",
+              "line-height",
+              "letter-spacing",
+              "margin",
+              "margin-top",
+              "margin-left",
+              "margin-bottom",
+              "margin-right",
+              "padding",
+              "padding-top",
+              "padding-left",
+              "padding-bottom",
+              "padding-right",
+              "border-radius",
+              "width",
+              "max-width",
             ],
             selectorBlackList: [],
             replace: true,
